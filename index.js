cde43c571e2366bff9d7402492939c806cb65353
Minh_Nhut <nhutp49@gmail.com>
2022-04-04 18:01:58 +0700

    end

---
+++ index.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("Thanh Cong"))
+.catch((err) => console.log(console.log(err)));
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, ()=> {
+    console.log("backend");
+
+})
